57bd8ba77215a3d3484f85830334890281cce62a
Jon Sjothun <jon@sjothun.com>
2015-06-22 22:10:07 -0700

    updated collision detection

--- js/app.js
+++ js/app.js
@@ -1,93 +1,103 @@
+"use strict";
 // Enemies our player must avoid
 var Enemy = function(row) {
-    this.x = -100; // initial x coordinate
-    this.y = (row * 83) + 63; // initial y coordinate. The water takes 63 pixels and each row after that take 83.
-// The image/sprite for our enemies, this uses
-// a helper we've provided to easily load images
+    // initial x coordinate. The enemy sprite is 101 pixels. This places them just off the canvas.
+    this.x = -100;
+    // initial y coordinate.  The offset for the first row (water) takes 63 pixels.
+    // Each row after that take 83 pixels to align the enemy bug in each path correctly.
+    this.y = (row * 83) + 63;
+// The image/sprite for our enemies, this use a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.speed = Math.floor(Math.random() * maxEnemySpeed) + 1; // randomly assign the speed for this instance of the enemy
 };
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position. The parameter dt is the time between 'ticks' of the game engine
+// It is passed as a multiplier to equalize the speed across browsers
 Enemy.prototype.update = function(dt) {
-// You should multiply any movement by the dt parameter
-// which will ensure the game runs at the same speed for
-// all computers
-    if (this.x > 505) { // if the enemy has crossed the screen then
-        this.x = -100; // move that enemy back to the left side
+    if (this.x > 505) { // if the enemy has crossed the screen (x coord is the width of the canvas) then
+        this.x = -100; // move that enemy back to the left side so they are just off the canvas
     } else {
+        // 'move' the enemy to the right, factoring the assigned speed, the host machine speed (dt)
+        // and the overall speed multiplier applied to this instance of the game. See README.md for info on gameSpeed
         this.x = this.x + (Math.abs(this.speed*dt)*gameSpeed); // move the enemy to the right
     }
 };
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-// the Player class with update(), render() and handleInput() methods.
+// the Player class with additional update(), render() and handleInput() methods.
 var Player = function() {
-// Variables applied to each of our instances go here,
-// we've provided one for you to get started
-    this.x = 202; // initial x coordinate
-    this.y = 415; // initial y coordinate
+// set the initial coordinates for the player to the starting position
+    this.reset();
+// assign the character to the player sprite
     this.sprite = 'images/char-boy.png';
 };
+
+// function checks if the player has reached the water or whether the player has 'collided' with an enemy
 Player.prototype.update = function() {
-// check if the player has reached the water or whether the player has 'collided' with an enemy
-    if (this.y === 0) { // the player made it across so we'll set the reset flag
+    if (this.y === -20) { // the player made it across (top of canvas) so reset their position
         this.reset();
     } else {
-        for (var i = 0; i < maxEnemies; i++) { // check each Enemy in the array
+        for (var i = 0; i < maxEnemies; i++) { // check against each Enemy in the array
             // NOTE: there is a 20 pixel offset of y coord of enemy and player relative to the "row" they occupy
             // for the purposes of collision detection.
-            if (this.y == allEnemies[i].y + 20) {
-                // if the player is on the same row as the Enemy we are checking in the array then
-                // the right hand side of the image collides before the y coord
-                //var closeEnough = (this.x - allEnemies[i].x); // how far away is the player from the enemy
-                if ((this.x - allEnemies[i].x) > 0 && (this.x - allEnemies[i].x) < 75){
+            if (this.y == allEnemies[i].y) {
+                // if the player is on the same row (same y coord.) as the Enemy instance then
+                // if there is any overlap between the player and enemy (collision!)then reset the player
+                // the 16 and 85 are because of 16 pixels of whitespace either side of the player image
+                // and the 101 is the width of the enemy image.
+                if ((allEnemies[i].x >= (this.x + 16) && allEnemies[i].x <= (this.x + 85)) 
+					|| ((allEnemies[i].x + 101) >= (this.x + 16) && (allEnemies[i].x + 101) <= (this.x + 85)))
+				{
                     this.reset();
                 }
             }
         }
     }
 };
+// simple rendering function to draw the player instance
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+// Checks which key was pressed and checks whether the player has reached the boundary in that direction
+// if not, the position of the player is updated, moving them in the direction indicated by the key pressed
 Player.prototype.handleInput = function(arrowKey) {
 // handle the input
     switch (arrowKey) {
         case "up":
-            if (this.y > 0) {this.y = this.y - 83};
+            if (this.y > -20) {this.y = this.y - 83;}
             break;
         case "down":
-            if (this.y < 415) {this.y = this.y + 83};
+            if (this.y < 395) {this.y = this.y + 83;}
             break;
         case "left":
-            if (this.x > 0) {this.x = this.x - 101};
+            if (this.x > 0) {this.x = this.x - 101;}
             break;
         case "right":
-            if (this.x < 404) {this.x = this.x + 101};
+            if (this.x < 404) {this.x = this.x + 101;}
             break;
     }
 };
+// the reset function places the instance of the player back at the starting position
 Player.prototype.reset = function(){
     this.x = 202; // initial x coordinate
-    this.y = 415; // initial y coordinate
+    this.y = 395; // initial y coordinate
 };
+
+// here is where the global variables and player and enemy instances are declared
 var maxEnemies = 3; // set the maximum number of enemies. One per row for now
-// these two variables can be edited to change the top speed of the enemies and the overall speed of the game
+// these two variables can be edited to change the max speed of an enemy bugs and the overall speed of the game
 var maxEnemySpeed = 5; // specify the max speed an enemy can move (1=slow, 2=normal, 3=fast)
 var gameSpeed = 50; // multiplier to dictate the speed that the enemies move
-// Now instantiate your objects.
 // Array object for all enemy objects, called allEnemies
 var allEnemies = [];
 for (var i = 0; i < maxEnemies; i++) {
     allEnemies.push(new Enemy(i));
 }
-// Place the instance of the player object in a variable called player
+// Create a new instance of the player object in a variable called player
 var player = new Player();
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Event listener capture the key press (release) and maps and passes the string
+// indicating which directional key was pressed by the user
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
